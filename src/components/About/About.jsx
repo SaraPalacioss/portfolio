19b596bea662b592f56caf5329f5ab361f4765ac
sarastackhouse <sarapalacios7@gmail.com>
2021-01-24 17:25:23 +0100

    First commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,82 @@
+import React, { useContext, useState, useEffect } from 'react';
+import Fade from 'react-reveal/Fade';
+import { Container, Row, Col } from 'react-bootstrap';
+import Title from '../Title/Title';
+import AboutImg from '../Image/AboutImg';
+import PortfolioContext from '../../context/context';
+import '../../style/sections/general.css';
+
+const About = () => {
+  const { about } = useContext(PortfolioContext);
+  const { img, paragraphOne, paragraphTwo, paragraphThree, img2, resume, resume2 } = about;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id="about">
+      <Container>
+        <Title title="README.MD" />
+        <Row className="about-wrapper">
+          <Col md={6} sm={12}>
+            <Fade left duration={1000} delay={600} distance="30px">
+              <div className="about-wrapper__image">
+                <AboutImg alt="profile picture" filename={img} className="photo"/>
+              </div>
+            </Fade>
+          </Col>
+          <Col md={6} sm={12}>
+            <Fade right={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <div className="about-wrapper__info">
+                <p className="about-wrapper__info-text">
+                  {paragraphOne ||
+                    `After working for more than 10 years as technician for IT companies I found in Web Development a real passion. I'm a technology lover and I do enjoy achieving new skills that let me find the better way to solve challenging issues. The learning process never ends!`}
+                </p>
+                <p className="about-wrapper__info-text text-style">
+                  {paragraphTwo ||
+                    'Tech stack'}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphThree || 'JavaScript | React | NodeJS | Express | MongoDB | Mongoose | Handlebars | API REST | Git | GitHub | ES6 | HTML | CSS'}
+                </p>
+              
+                {resume && (
+                  <span className="d-flex mt-3">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn--resume"
+                      href={resume}
+                    >
+                      Resume ENG
+                    </a>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn--resume"
+                      href={resume2}
+                    >
+                      Resume ES
+                    </a>
+                  </span>
+                )}
+                          </div>
+            </Fade>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/mock/data.js
@@ -0,0 +1,92 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: 'Sara Palacios | Portfolio', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my portfolio', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: '<Hi, I Am',
+  name: 'Sara Palacios',
+  subtitle: `FullStack Developer/>`,
+  cta: '+',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'profile.jpg',
+  paragraphOne: `After working for more than 10 years as technician for IT companies I found in Web Development a real passion. I'm a technology lover and I do enjoy achieving new skills that let me find the better way to solve challenging issues. The learning process never ends! `,
+  paragraphTwo: `Tech stack:`,
+  paragraphThree: 'JavaScript | React | NodeJS | Express | MongoDB | Mongoose | Handlebars | API REST | Git | GitHub | ES6 | HTML | CSS.',
+  resume: 'https://drive.google.com/file/d/1WPQMTadC6JiBGIMbZgcU_vQmVsVAwspp/view?usp=sharing', 
+  resume2: 'https://drive.google.com/file/d/1hR2TtqO3df5LNl7UUCySIwEB-cIBEMT2/view?usp=sharing'// if no resume, the button will not show up
+// if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'film.png',
+    title: 'theGoodFilm',
+    info: `This is my Ironhack final project. It's a full responsive MERN web application make with ReactJs,
+    ExpressJs, NodeJs, MongoDB, Axios, API Rest & Bootstrap.`,
+    info2: '',
+    url: 'https://thegoodfilm.netlify.app/',
+    repo: 'https://github.com/thegoodfilm', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'cocktails.png',
+    title: 'All City Cocktails',
+    info: `Full stack project using ExpressJs, NodeJs, Handlebars, MongoDB, Axios, API Rest & Bootstrap made in 1 week for Ironhack bootcamp`,
+    info2: '',
+    url: 'https://all-city-cocktails.herokuapp.com/',
+    repo: 'https://github.com/SaraPalacioss/project-all-city-cocktails', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'Funko.png',
+    title: 'Funko Hollywood Trivial',
+    info: `Front End project made in just 7 days for Ironhack bootcamp and developed with Vanilla JavaScript, DOM manipulation, HTML & CSS.`,
+    info2: '',
+    url: 'https://sarapalacioss.github.io/funko-hollywood-trivial',
+    repo: 'https://github.com/SaraPalacioss/funko-hollywood-trivial', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: 'Would you like to contact with me? Awesome!',
+  // btn: '+34 601327667',
+  email: 'contact@sarapalacios.me',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/sara-palaciosdepedro',
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: 'https://github.com/sarapalacioss',
+    },
+    {
+      id: nanoid(),
+      name: 'codepen',
+      url: 'https://codepen.io/sarapalacios',
+    }
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
+};
